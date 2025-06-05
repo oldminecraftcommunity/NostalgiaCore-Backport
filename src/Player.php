381063f8f78d839a0a079266fb2cf16506d2b4d7
GameHerobrine <waterfall009@gmail.com>
2025-06-05 14:23:38 +0300

    always get level spawn when respawning instead of using spawnposition

--- src/Player.php
+++ src/Player.php
@@ -2422,7 +2422,7 @@ class Player{
 				$this->craftingType = CraftingRecipes::TYPE_INVENTORY;
 				
 				$this->checkSpawnPosition();
-				$tpTarget = $this->spawnPosition;
+				$tpTarget = $this->level->getSpawn();
 				if($this->bedPosition != null){
 					[$levelname, $x, $y, $z] = $this->bedPosition;
 					$level = $this->server->api->level->get($levelname);
@@ -2430,7 +2430,6 @@ class Player{
 						remove_bed:
 						$this->sendChat("The bed is missing or cannot be accessed.");
 						$this->setBedPosition(null);
-						$this->setSpawn($this->level->getSpawn());
 					}else{
 						$spawnPoint = BedBlock::findStandUpPosition($level, $x, $y, $z);
 						if($spawnPoint == null) goto remove_bed;
